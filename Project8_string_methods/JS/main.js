e344af8cae0ab04371dcb98e48cec614436cf4b6
Derya Seyidov <deryakrdnz1996@gmail.com>
2026-02-03 18:19:32 -0500

    Project 8 String & Number Methods Assignment

--- Project8_string_methods/JS/main.js
+++ Project8_string_methods/JS/main.js
@@ -1,7 +1,33 @@
-let text1 = "Hello ";
-let text2 = "JavaScript ";
-let text3 = "World!";
+// CONCAT METHOD
+// This function combines multiple strings into one
+function useConcat() {
+    let part1 = "Hello ";
+    let part2 = "JavaScript ";
+    let part3 = "World!";
+    let result = part1.concat(part2, part3);
+    document.getElementById("concat").innerHTML = result;
+}
 
-let finalText = text1.concat(text2, text3);
+// SLICE METHOD
+// This function extracts a section of a string
+function useSlice() {
+    let text = "Hello JavaScript World!";
+    let result = text.slice(6, 16);
+    document.getElementById("slice").innerHTML = result;
+}
 
-document.getElementById("result").innerHTML = finalText;
+// toString METHOD
+// This function converts a number to a string
+function useToString() {
+    let number = 100;
+    let result = number.toString();
+    document.getElementById("toString").innerHTML = result;
+}
+
+// toPrecision METHOD
+// This function formats a number to a specified length
+function useToPrecision() {
+    let number = 123.456;
+    let result = number.toPrecision(5);
+    document.getElementById("toPrecision").innerHTML = result;
+}
